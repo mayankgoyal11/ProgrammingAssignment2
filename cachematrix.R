99beaf42461a2a24c3d01478941fffb1617553c8
mayankgoyal11 <mayankgoyal11@gmail.com>
2017-06-22 00:19:30 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The combination of the below two functions are used to
+## calculate the inverse of a matrix if not available in the
+## cache. If it is available in the cache it is retreived from
+## there
 
-## Write a short comment describing this function
+## This function contains a series of functions used to either 
+## store the inverse matrix or return the cached inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i<-NULL
+        getD<-function() x
+        setInverse<-function(inv) i <<- inv
+        getInverse<-function()i
+        list(getD=getD,getInverse=getInverse,setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
+## This function looks for the inverse of a matrix in the cache and
+## if not availabe calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         i<-x$getInverse()
+        if (!is.null(i)){
+                message("Invoked Cache")
+                return(i)
+        }
+        data<-x$getD()
+        if (nrow(data)==ncol(data))
+        {i<-solve(data)
+        
+        }else
+        {
+                message("Matrix not Square")
+        }
+        x$setInverse(i)
+        i
 }
